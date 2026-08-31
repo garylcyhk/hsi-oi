8a966511d5210a838c68a2eb2aee96960e88fea8
garylcyhk <garylcyhk@gmail.com>
2026-09-01 00:07:48 +0800

    Refresh calendar snapshot with this-week forecasts

--- calendar/data.js
+++ calendar/data.js
@@ -1,34 +1,29 @@
 window.FF_CAL = {
-  asOf: "2026-09-01",
-  tz: "HKT",
-  source: "https://www.forexfactory.com/calendar",
-  sourceRange: "https://www.forexfactory.com/calendar?range=aug26.2026-aug31.2026",
-  rangeLabel: "8/26\u20138/31 \u56de\u9867 \u00b7 9/1\u20139/4 \u524d\u77bb",
-  note: "\u7cbe\u9078\u5c0d\u6052\u6307\u8f03\u6709\u95dc\u5605 CNY / USD / JPY \u6578\u64da\u3002\u5b8c\u6574\u65e5\u66c6\u8acb\u958b Forex Factory\u3002\u6642\u9593\u70ba\u9999\u6e2f\u6642\u9593\u3002",
-  lastWeek: [
-    { date:"2026-08-26", time:"20:30", ccy:"USD", impact:"high", name:"Core PCE m/m", actual:"0.2%", forecast:"0.2%", previous:"0.1%" },
-    { date:"2026-08-26", time:"20:30", ccy:"USD", impact:"high", name:"Prelim GDP q/q", actual:"1.5%", forecast:"1.5%", previous:"1.5%" },
-    { date:"2026-08-26", time:"20:30", ccy:"USD", impact:"med", name:"Durable Goods Orders m/m", actual:"1.1%", forecast:"0.4%", previous:"0.5%" },
-    { date:"2026-08-26", time:"20:30", ccy:"USD", impact:"med", name:"Personal Spending m/m", actual:"0.2%", forecast:"0.1%", previous:"0.3%" },
-    { date:"2026-08-27", time:"20:30", ccy:"USD", impact:"high", name:"Unemployment Claims", actual:"203K", forecast:"208K", previous:"207K" },
-    { date:"2026-08-27", time:"20:30", ccy:"USD", impact:"med", name:"Goods Trade Balance", actual:"-118.8B", forecast:"-100.8B", previous:"-101.4B" },
-    { date:"2026-08-27", time:"21:00", ccy:"CNY", impact:"low", name:"CB Leading Index m/m", actual:"-0.3%", forecast:"\u2014", previous:"0.1%" },
-    { date:"2026-08-27", time:"\u2014", ccy:"USD", impact:"high", name:"Jackson Hole Symposium D1", actual:"\u2014", forecast:"\u2014", previous:"\u2014" },
-    { date:"2026-08-28", time:"04:30", ccy:"JPY", impact:"high", name:"Tokyo Core CPI y/y", actual:"1.8%", forecast:"1.8%", previous:"1.7%" },
-    { date:"2026-08-28", time:"19:45", ccy:"USD", impact:"high", name:"Chicago PMI", actual:"47.1", forecast:"57.9", previous:"57.6" },
-    { date:"2026-08-28", time:"20:00", ccy:"USD", impact:"high", name:"Fed Chair Warsh Speaks", actual:"\u2014", forecast:"\u2014", previous:"\u2014" },
-    { date:"2026-08-28", time:"20:00", ccy:"USD", impact:"med", name:"Revised UoM Sentiment", actual:"51.7", forecast:"51.0", previous:"51.0" },
-    { date:"2026-08-31", time:"09:30", ccy:"CNY", impact:"high", name:"Manufacturing PMI", actual:"49.8", forecast:"49.5", previous:"49.2" },
-    { date:"2026-08-31", time:"09:30", ccy:"CNY", impact:"high", name:"Non-Manufacturing PMI", actual:"49.0", forecast:"49.5", previous:"49.0" }
+  "asOf": "2026-09-01 00:06",
+  "tz": "HKT",
+  "source": "https://www.forexfactory.com/calendar",
+  "sourceRange": "https://www.forexfactory.com/calendar?week=this",
+  "rangeLabel": "09-01\u201309-04 \u672c\u9031 \u00b7 \u81ea\u52d5\u66f4\u65b0",
+  "note": "\u6bcf\u65e5\u81ea\u52d5\u66f4\u65b0\u672c\u9031\uff08FF this-week JSON\uff09\u3002\u4e0b\u9031\u6703\u5728\u9031\u65e5\uff0f\u4e00\u63db\u9031\u5f8c\u51fa\u73fe\u3002\u7cbe\u9078 USD/CNY/JPY/HKD/AUD \u4e2d\u9ad8\u5f71\u97ff\u3002",
+  "lastWeek": [
+    {"date":"2026-08-26","time":"20:30","ccy":"USD","impact":"high","name":"Core PCE m/m","actual":"0.2%","forecast":"0.2%","previous":"0.1%"},
+    {"date":"2026-08-26","time":"20:30","ccy":"USD","impact":"high","name":"Prelim GDP q/q","actual":"1.5%","forecast":"1.5%","previous":"1.5%"},
+    {"date":"2026-08-26","time":"20:30","ccy":"USD","impact":"med","name":"Durable Goods Orders m/m","actual":"1.1%","forecast":"0.4%","previous":"0.5%"},
+    {"date":"2026-08-27","time":"20:30","ccy":"USD","impact":"high","name":"Unemployment Claims","actual":"203K","forecast":"208K","previous":"207K"},
+    {"date":"2026-08-28","time":"19:45","ccy":"USD","impact":"high","name":"Chicago PMI","actual":"47.1","forecast":"57.9","previous":"57.6"},
+    {"date":"2026-08-31","time":"09:30","ccy":"CNY","impact":"high","name":"Manufacturing PMI","actual":"49.8","forecast":"49.5","previous":"49.2"},
+    {"date":"2026-08-31","time":"09:30","ccy":"CNY","impact":"high","name":"Non-Manufacturing PMI","actual":"49.0","forecast":"49.5","previous":"49.0"}
   ],
-  thisWeek: [
-    { date:"2026-09-01", time:"22:00", ccy:"USD", impact:"high", name:"ISM Manufacturing PMI", actual:"", forecast:"\u2014", previous:"\u2014" },
-    { date:"2026-09-01", time:"22:00", ccy:"USD", impact:"high", name:"JOLTS Job Openings", actual:"", forecast:"\u2014", previous:"\u2014" },
-    { date:"2026-09-01", time:"17:00", ccy:"EUR", impact:"high", name:"Eurozone CPI Flash", actual:"", forecast:"\u2014", previous:"\u2014" },
-    { date:"2026-09-02", time:"20:15", ccy:"USD", impact:"high", name:"ADP Employment Change", actual:"", forecast:"\u2014", previous:"\u2014" },
-    { date:"2026-09-02", time:"21:45", ccy:"CAD", impact:"high", name:"BoC Rate Decision", actual:"", forecast:"\u2014", previous:"\u2014" },
-    { date:"2026-09-02", time:"02:00", ccy:"USD", impact:"med", name:"Beige Book", actual:"", forecast:"\u2014", previous:"\u2014" },
-    { date:"2026-09-03", time:"22:00", ccy:"USD", impact:"high", name:"ISM Services PMI", actual:"", forecast:"\u2014", previous:"\u2014" },
-    { date:"2026-09-04", time:"20:30", ccy:"USD", impact:"high", name:"Non-Farm Payrolls / \u5931\u696d\u7387", actual:"", forecast:"\u2014", previous:"Jul -23K / 4.1%" }
+  "thisWeek": [
+    {"date":"2026-09-01","time":"22:00","ccy":"USD","impact":"high","name":"ISM Manufacturing PMI","actual":"","forecast":"55.2","previous":"55.6"},
+    {"date":"2026-09-01","time":"22:00","ccy":"USD","impact":"med","name":"ISM Manufacturing Prices","actual":"","forecast":"71.2","previous":"71.1"},
+    {"date":"2026-09-01","time":"22:00","ccy":"USD","impact":"med","name":"JOLTS Job Openings","actual":"","forecast":"7.33M","previous":"7.36M"},
+    {"date":"2026-09-02","time":"09:30","ccy":"AUD","impact":"high","name":"GDP q/q","actual":"","forecast":"0.3%","previous":"0.3%"},
+    {"date":"2026-09-02","time":"20:15","ccy":"USD","impact":"med","name":"ADP Non-Farm Employment Change","actual":"","forecast":"47K","previous":"44K"},
+    {"date":"2026-09-03","time":"20:30","ccy":"USD","impact":"med","name":"Unemployment Claims","actual":"","forecast":"205K","previous":"203K"},
+    {"date":"2026-09-03","time":"22:00","ccy":"USD","impact":"med","name":"ISM Services PMI","actual":"","forecast":"54.1","previous":"54.1"},
+    {"date":"2026-09-04","time":"20:30","ccy":"USD","impact":"high","name":"Average Hourly Earnings m/m","actual":"","forecast":"0.3%","previous":"0.1%"},
+    {"date":"2026-09-04","time":"20:30","ccy":"USD","impact":"high","name":"Non-Farm Employment Change","actual":"","forecast":"55K","previous":"-23K"},
+    {"date":"2026-09-04","time":"20:30","ccy":"USD","impact":"high","name":"Unemployment Rate","actual":"","forecast":"4.1%","previous":"4.1%"}
   ]
 };
